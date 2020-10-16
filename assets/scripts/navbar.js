17cf84c7131503881bb302b92207420d4f337982
Mahdi Karimi <mhdi_kr@outlook.com>
2020-10-16 12:15:41 +0330

    blog page

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,6 +1,32 @@
-window.onscroll = function() { myFunction() };
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
+
+window.onscroll = function() {myFunction()};
+const navbar = document.getElementById("nav-wrapper");
+const sticky = navbar.offsetTop;
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
@@ -8,3 +34,4 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
